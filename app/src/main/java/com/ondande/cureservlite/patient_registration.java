37fafe96034b0954e4c4a453a6bd87a567d26848
OndAnde <pashamanulik@gmail.com>
2019-03-22 16:33:30 +0300

    full registration + firebase database

--- app/src/main/java/com/ondande/cureservlite/patient_registration.java
+++ app/src/main/java/com/ondande/cureservlite/patient_registration.java
@@ -1,33 +1,13 @@
 package com.ondande.cureservlite;
 
-import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.rengwuxian.materialedittext.MaterialEditText;
-import com.szagurskii.patternedtextwatcher.PatternedTextWatcher;
-
 public class patient_registration extends AppCompatActivity {
-
-    MaterialEditText edt_name;
-    MaterialEditText edt_surname;
-    MaterialEditText edt_phone;
-    MaterialEditText edt_address;
-    MaterialEditText edt_birth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_registration);
-
-        edt_name = (MaterialEditText)findViewById(R.id.edt_name);
-        edt_surname = (MaterialEditText)findViewById(R.id.edt_surname);
-        edt_phone = (MaterialEditText)findViewById(R.id.edt_phone);
-        edt_address = (MaterialEditText)findViewById(R.id.edt_address);
-        edt_birth = (MaterialEditText)findViewById(R.id.edt_birth);
-
-        MaterialButton btn_conf_patient = (MaterialButton)findViewById(R.id.btn_conf_patient);
-
-        edt_birth.addTextChangedListener(new PatternedTextWatcher("####-##-##"));
     }
 }
--- app/src/main/java/com/ondande/cureservlite/user_sign_up.java
+++ app/src/main/java/com/ondande/cureservlite/user_sign_up.java
@@ -1,62 +1,67 @@
 package com.ondande.cureservlite;
 
 import android.content.Intent;
-import android.support.design.button.MaterialButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
-import com.rengwuxian.materialedittext.MaterialEditText;
-import com.szagurskii.patternedtextwatcher.PatternedTextWatcher;
+public class user_sign_up extends AppCompatActivity {
 
-public class user_sign_up extends AppCompatActivity implements View.OnClickListener {
+    Button btn_doc;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
+        btn_doc = (Button)findViewById(R.id.btn_doc);
+        btn_doc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
 
-        findViewById(R.id.btn_doc).setOnClickListener(this);
-        findViewById(R.id.btn_patient).setOnClickListener(this);
+                Intent reg;
+                reg = new Intent(user_sign_up.this,doc_registration.class);
+                startActivity(reg);
+
+            }
+        });
+//        findViewById(R.id.btn_patient).setOnClickListener(this);
     }
 
 
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId())
-        {
-            case R.id.btn_doc:
-                startDocReg();
-                break;
-
-            case R.id.btn_patient:
-                startPatientReg();
-                break;
-
-        }
-
-    }
-
-    public void startDocReg(){
-
-        Intent reg;
-        reg = new Intent(user_sign_up.this,doc_registration.class);
-        this.startActivity(reg);
-
-    }
+//    @Override
+//    public void onClick(View view) {
+//
+//        switch (view.getId())
+//        {
+//            case R.id.btn_doc:
+//                startDocReg();
+//                break;
+//
+//            case R.id.btn_patient:
+//                startPatientReg();
+//                break;
+//
+//        }
+//
+//    }
+//
+//    public void startDocReg(){
+//
+//        Intent reg;
+//        reg = new Intent(user_sign_up.this,doc_registration.class);
+//        startActivity(reg);
+//
+//    }
 
     public void startPatientReg(){
 
         Intent reg;
         reg = new Intent(user_sign_up.this,patient_registration.class);
-        this.startActivity(reg);
+        startActivity(reg);
 
     }
-}
 
+}
